ab364e2cb7c3c431ec73958af18d0071fc83e52b
Ghost <xxyyzzghost@gmail.com>
2018-11-22 22:39:14 +0200

    Sass installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
- 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -18,7 +19,20 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
             // ...additional rules...
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
 };
