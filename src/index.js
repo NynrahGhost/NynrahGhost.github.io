0c5c1133a034fc3c02722e6b0af20843743d00fc
Ghost <xxyyzzghost@gmail.com>
2018-12-09 00:14:34 +0200

    Started making front + several ajax requests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 import './scss/main.scss';
+import 'bootstrap';
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
 console.log('Hello!');
 console.log(`The time is ${new Date()}`);
+
+//let _makeProduct = require('./modules/product-html');
+
+jQuery.ajax({
+    url: 'http://nit.tron.net.ua/api/category/list',
+    method: 'get',
+    dataType: 'json',
+    success: function(json){
+        console.log('Loaded via AJAX!');
+        console.table(json);
+        let i = 1;
+        json.forEach(
+                ({id, name, description,}) => {
+                    if (i % 3 == 0) {
+                        $('.category-list').append($(`<div class="row">`))
+                        console.log('DONE!!!');
+                    }
+                    $('.category-list').append($(`<a href="#" class="list-group-item list-group-item-action" category-id="${id}">${name}
+                    <p class="mb-1">${description}</p></a>`))
+                    if (i % 3 == 0) {
+                        $('.category-list').append($(`</div>`))
+                    }
+                    i += 1;
+                }
+        );
+        console.log('Added to grid');
+    },
+
+    error: function(xhr){
+        alert("An error occured: " + xhr.status + " " + xhr.statusText);
+    },
+});
+
+jQuery.ajax({
+    url: 'https://nit.tron.net.ua/api/product/list',
+    method: 'get',
+    dataType: 'json',
+    success: function(json){
+        console.log('Loaded via AJAX!');
+        console.table(json);
+        json.forEach(({id, name, image_url, description, price,}) => {
+            /*if (id % 3 == 0) {
+                $('.product-grid').append($(`</div>`))
+                $('.product-grid').append($(`<div class="row">`))
+            }*/
+            $('.product-grid > .row').append($(`<div class="col-sm-4" data-product-id="${id}"><div class="panel panel-primary">
+            <div class="panel-heading">${name}</div>
+            <div class="panel panel-body" style="overflow: hidden;">
+                <img src="${image_url}" alt="${name}" class="img-fluid product-image"></div>
+            <div class="panel-footer" >${description}</div>
+            </div></div>`));
+        });
+        console.log('Added to grid');
+    },
+
+    error: function(xhr){
+        alert("An error occured: " + xhr.status + " " + xhr.statusText);
+    },
+
+});
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
@@ -22,17 +23,55 @@ module.exports = {
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    MiniCssExtractPlugin.loader, /*{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        plugins: function () { // post css plugins, can be exported to postcss.config.js
+                            return [
+                                require('precss'),
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }*/
                     'css-loader',
-                    'sass-loader',
-                ],
+                    'sass-loader'
+                ]
+
             },
+            {
+                // Exposes jQuery for use outside Webpack build
+                test: require.resolve('jquery'),
+                use: [{
+                    loader: 'expose-loader',
+                    options: 'jQuery'
+                },{
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            }
             // ...additional rules...
         ],
     },
     plugins: [
+        require('autoprefixer'),
         new MiniCssExtractPlugin({
             filename: "css/[name].css",
         }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
     ],
+    resolve: {
+        alias: {
+            jquery: "jquery/src/jquery"
+        }
+    }
 };
